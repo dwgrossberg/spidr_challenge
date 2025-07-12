a34c530144e2f0ba5ba826dea28731a5d00a894f
Daniel <dwgrossberg@gmail.com>
2025-07-11 22:14:26 -0500

    Setup infra

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import DoodleBackground from "./components/DoodleBackground";
+
+function App() {
+  return (
+    <div className="App">
+      <DoodleBackground />
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
